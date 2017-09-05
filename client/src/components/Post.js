b0e994a2aa4a954a3ee40bc67d34b405fcceb9f0
paulakedouk <paulakedouk@gmail.com>
2017-09-05 13:49:09 -0700

    Adjust style grid

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,10 +5,13 @@ import React, { Component } from 'react';
 export default class Post extends Component {
   render() {
     return (
-      <div className="Post">
+      <div className="post">
         <h1>The top 10 things to do in San Francisco 2017</h1>
         <h2>Today by Paula in Redux</h2>
-        <p>Nonononononononononononononoo…</p>
+        <span>
+          Nonononononononononononononon ononononon ononononononononononononononono nonononononononononononononon
+          onononononononononononononono nonononononononono…
+        </span>
       </div>
     );
   }
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-
 import Post from './Post';
 
 export default class PostList extends Component {
   render() {
     return (
-      <div className="postlist">
-        <div className="postlist-item">
-          <Post />
-        </div>
-        <div className="postlist-item">
-          <Post />
-        </div>
-        <div className="postlist-item">
-          <Post />
-        </div>
-        <div className="postlist-item">
-          <Post />
-        </div>
-        <div className="postlist-item">
-          <Post />
-        </div>
-        <div className="postlist-item">
-          <Post />
+      <div className="postlist-container">
+        <div className="postlist-table">
+          <div className="cel">
+            <Post />
+          </div>
+          <div className="cel">
+            <Post />
+          </div>
+          <div className="cel">
+            <Post />
+          </div>
+          <div className="cel">
+            <Post />
+          </div>
+          <div className="cel">
+            <Post />
+          </div>
+          <div className="cel">
+            <Post />
+          </div>
         </div>
       </div>
     );
